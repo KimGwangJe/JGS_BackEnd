5f69f0513959d045366c9d7ac675c679dadd06c6
Kim Gwang Je <gwangjeg14@gmail.com>
2024-08-06 18:50:50 +0900

    first setting

---
+++ src/main/java/com/example/JustGetStartedBackEnd/JustGetStartedBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.JustGetStartedBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustGetStartedBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustGetStartedBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JustGetStartedBackEnd/JustGetStartedBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JustGetStartedBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustGetStartedBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
